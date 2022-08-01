79c6d99d6c52707759cecc763ee5ddb73118db28
kenmaready <ken@maready.com>
2022-08-01 08:15:55 -0400

    Initial Commit

---
+++ ML-Hotdog-Classifier/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ML-Hotdog-Classifier
+//
+//  Created by Ken Maready on 8/1/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
