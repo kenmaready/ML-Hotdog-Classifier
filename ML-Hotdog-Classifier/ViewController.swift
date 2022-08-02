65d92d7b16008cfda417adc54a7d9b034ffce0a4
kenmaready <ken@maready.com>
2022-08-02 08:28:22 -0400

    ML Model connected and working to identify hot dog vs. not

--- ML-Hotdog-Classifier/ViewController.swift
+++ ML-Hotdog-Classifier/ViewController.swift
@@ -17,13 +17,14 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         imagePickerSetup()
-
     }
-
     
 }
 
+// MARK: - Image Selection and Display
+
 extension ViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
     func imagePickerSetup() {
         imagePicker.delegate = self
         if UIImagePickerController.isSourceTypeAvailable(.camera) {
@@ -43,6 +44,12 @@ extension ViewController: UIImagePickerControllerDelegate, UINavigationControlle
         
         if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
             mainImageView.image = image
+            
+            guard let ciimage = CIImage(image: image) else {
+                fatalError("Could not convert selected image to CIImage")
+            }
+            
+            detect(ciimage)
         }
        
         imagePicker.dismiss(animated: true, completion: nil)
@@ -50,3 +57,37 @@ extension ViewController: UIImagePickerControllerDelegate, UINavigationControlle
     
 }
 
+// MARK: - Machine Learning (CoreML)
+
+extension ViewController {
+    func detect(_ image: CIImage) {
+        guard let model = try? VNCoreMLModel(for: Inceptionv3().model) else {
+            fatalError("Error occurred during instantiation of MLModel")
+        }
+        
+        let request = VNCoreMLRequest(model: model) { request, error in
+            guard let results = request.results as? [VNClassificationObservation] else {
+                fatalError("Error occurred during retrieval of results from MLModel analysis")
+            }
+            
+            print("results: \(results)")
+            
+            if let firstResult = results.first {
+                if firstResult.identifier.contains("hotdog") {
+                    self.navigationItem.title = "Hot Dog!"
+                } else {
+                    self.navigationItem.title = "Not a Hot Dog"
+                }
+            }
+        }
+        
+        let handler = VNImageRequestHandler(ciImage: image)
+        
+        do {
+            try handler.perform([request])
+        } catch {
+            print("Error occurred during requesting analysis from MLModel")
+        }
+        
+    }
+}
