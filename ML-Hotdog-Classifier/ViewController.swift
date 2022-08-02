c1032f9f67e05b43bf059d8f05ef56af631ec9e8
kenmaready <ken@maready.com>
2022-08-02 07:12:49 -0400

    Image selection and display features working

--- ML-Hotdog-Classifier/ViewController.swift
+++ ML-Hotdog-Classifier/ViewController.swift
@@ -6,14 +6,47 @@
 //
 
 import UIKit
+import CoreML
+import Vision
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var mainImageView: UIImageView!
+    let imagePicker = UIImagePickerController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        imagePickerSetup()
+
     }
 
+    
+}
 
+extension ViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    func imagePickerSetup() {
+        imagePicker.delegate = self
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            imagePicker.sourceType = .camera
+        } else {
+            print("Camera is not available on this device/simulator.")
+            imagePicker.sourceType = .photoLibrary
+        }
+        imagePicker.allowsEditing = false
+    }
+    
+    @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            mainImageView.image = image
+        }
+       
+        imagePicker.dismiss(animated: true, completion: nil)
+    }
+    
 }
 
